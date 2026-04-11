9bf2c5c46c8038407a6243648e15be467935f088
dportc01 <dportc01@estudiantes.unileon.es>
2026-04-11 19:10:29 +0200

    Add tailwindcss and removed unused files

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,21 +1,21 @@
 import { useEffect, useState } from "react";
-import "./HomePage.css";
+import "./index.css";
 
 function HomePage() {
   return (
-    <>
+    <div>
       <Banner />
-      <div className="main-view">
+      <div className="flex flex-row">
         <Sidebar />
         <Content />
       </div>
-    </>
+    </div>
   );
 }
 
 function Banner() {
   return (
-    <div className="banner">
+    <div className="border-8 border-gray-300 flex justify-center text-4xl tracking-[0.7em] font-bold uppercase py-4">
       <h1>Secure View</h1>
     </div>
   );
@@ -23,19 +23,19 @@ function Banner() {
 
 function Sidebar() {
   return (
-    <div className="sidebar">
-      <h3>Página principal</h3>
-      <h3>Cámaras</h3>
-      <h3>Almacenamiento</h3>
-      <h3>Notificariones</h3>
-      <h3>Ajustes</h3>
+    <div className="bg-gray-300 w-fit px-4">
+      <h3 className="my-4 text-[18px]">Página principal</h3>
+      <h3 className="my-4 text-[18px]">Cámaras</h3>
+      <h3 className="my-4 text-[18px]">Almacenamiento</h3>
+      <h3 className="my-4 text-[18px]">Notificariones</h3>
+      <h3 className="my-4 text-[18px]">Ajustes</h3>
     </div>
   );
 }
 
 function Content() {
   return (
-    <div className="content">
+    <div className="flex flex-col flex-1 ml-8 mt-4 space-y-4">
       <TimeDisplay />
       <Notifications />
       <Video />
@@ -51,22 +51,20 @@ function TimeDisplay() {
       setTime(new Date().toLocaleString());
     }, 1000);
 
-    return () => {
-      clearInterval(interval);
-    };
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div>
-      <h2>Hora actual: {time}</h2>
+      <h2 className="text-2xl">Hora actual: {time}</h2>
     </div>
   );
 }
 
 function Notifications() {
   return (
-    <div className="notifications">
-      <h2>Últimas notificaciones</h2>
+    <div>
+      <h2 className="text-2xl">Últimas notificaciones</h2>
     </div>
   );
 }
@@ -74,14 +72,15 @@ function Notifications() {
 function Video() {
   return (
     <div>
-      <h2>Cámaras principales</h2>
+      <h2 className="text-2xl mb-2">Cámaras principales</h2>
 
       <img
         src="http://localhost:5000/video"
         alt="Camera Stream"
-        style={{ width: "640px", borderRadius: "8px" }}
+        className="w-[640px] rounded-lg"
       />
     </div>
   );
 }
+
 export default HomePage;
