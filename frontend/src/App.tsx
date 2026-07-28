1aa7806935354c67a9349f6030b34d029420d2d2
dportc01 <dportc01@estudiantes.unileon.es>
2026-07-28 17:10:01 +0200

    Small refactor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/Home";
 import Storage from "@/Storage";
 import Settings from "@/Settings";
 import Logging from "@/Logging";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/storage" element={<Storage />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/logging" element={<Logging />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/storage" element={<Storage />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/logging" element={<Logging />} />
+    </Routes>
   );
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.tsx";
@@ -8,10 +9,12 @@ import { TooltipProvider } from "@/components/ui/tooltip.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <TooltipProvider>
-        <App />
-      </TooltipProvider>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider>
+        <TooltipProvider>
+          <App />
+        </TooltipProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
